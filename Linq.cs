ce1afac9c5f2797f75ebf0128e8ef336f08c3d4b
Damir <125348732+ki55el@users.noreply.github.com>
2023-05-16 08:58:59 +0600

    Add files via upload

---
+++ Linq.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+class HelloWorld
+{
+    static void Main()
+    {
+        var nums = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+        Counter(nums);
+
+        Console.WriteLine("Удаление элементов . . . ");
+
+        nums = (from n in nums
+               where n > 0
+               select n).ToArray();
+        Counter(nums);
+    }
+    static void Counter(int[] nums) => Console.WriteLine($@"
+min: {nums.Min()}
+max: {nums.Max()}
+кол-во четных: {nums.Count(n => n % 2 == 0)}
+кол-во положительных: {nums.Count(n => n > 0)}
+кол-во ненулевых: {nums.Count(n => n != 0)}
+");
+}
