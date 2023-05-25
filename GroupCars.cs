1c54781a1b44d0a951f7cbb218354ec7e420026b
Damir <125348732+ki55el@users.noreply.github.com>
2023-05-25 17:56:08 +0600

    Update GroupCars.cs

--- GroupCars.cs
+++ GroupCars.cs
@@ -1,19 +1,19 @@
-﻿using System;
+using System;
 using System.Linq;
-using System.Xml.Linq;
+using System.Collections.Generic;
 
 record class Driver(string fname, string name, Car car);
-record class Car(int id, string mark, string color, DateTime realese);
+record class Car(string id, string mark, string color, DateTime realese);
 class HelloWorld
 {
     static void Main()
     {
         Driver[] drivers =
         {
-            new Driver("Ivanov", "Roman", new Car(1234, "GAZ", "blue", Convert.ToDateTime("12.09.2002"))),
-            new Driver("Syzdykov", "Damir", new Car(1345, "GAZ", "green", Convert.ToDateTime("15.11.2007"))),
-            new Driver("Ivanov", "Maksim", new Car(1456, "Toyota", "blue", Convert.ToDateTime("11.10.2000"))),
-            new Driver("Kiselyov", "Mikhail", new Car(1567, "BMW", "yellow", Convert.ToDateTime("10.12.2010")))
+            new Driver("Ivanov", "Roman", new Car("1234", "GAZ", "blue", Convert.ToDateTime("12.09.2002"))),
+            new Driver("Syzdykov", "Damir", new Car("1345", "GAZ", "green", Convert.ToDateTime("15.11.2007"))),
+            new Driver("Ivanov", "Maksim", new Car("1456", "Toyota", "blue", Convert.ToDateTime("11.10.2000"))),
+            new Driver("Kiselyov", "Mikhail", new Car("1567", "BMW", "yellow", Convert.ToDateTime("10.12.2010")))
         };
         while (true)
         {
@@ -28,42 +28,32 @@ class HelloWorld
             {
                 case "1":
                     var by_car = from driver in drivers
-                        group driver.fname by driver.car.id;
-                    foreach (var car in by_car)
-                    {
-                        Console.WriteLine(car.Key);
-                        foreach (var res in car)
-                        {
-                            Console.WriteLine('\t' + res);
-                        }
-                    }
+                                 group driver.fname by driver.car.id;
+                    Printer(by_car);
                     break;
 
                 case "2":
                     var by_mark = from driver in drivers
-                        group driver.car.color by driver.car.mark;
-                    foreach (var mark in by_mark)
-                    {
-                        Console.WriteLine(mark.Key);
-                        foreach (var res in mark)
-                        {
-                            Console.WriteLine('\t' + res);
-                        }
-                    }
+                                  group driver.car.color by driver.car.mark;
+                    Printer(by_mark);
                     break;
 
                 case "3":
                     var by_fullname = from driver in drivers
-                        group driver.name by driver.fname ;
-                    foreach (var fname in by_fullname)
-                    {
-                        Console.WriteLine(fname.Key);
-                        foreach (var res in fname)
-                        {
-                            Console.WriteLine('\t' + res);
-                        }
-                    }
+                                      group driver.name by driver.fname ;
+                    Printer(by_fullname);
                     break;
+            }
+        }
+    }
+    static void Printer(IEnumerable<IGrouping<string, string>> groups)
+    {
+        foreach (var group in groups)
+        {
+            Console.WriteLine(group.Key + ':');
+            foreach (var e in group)
+            {
+                Console.WriteLine('\t' + e);
             }
         }
     }
