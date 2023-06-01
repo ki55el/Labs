51c0d3dfcbb33ac74697eb2724e5c02c6b82a7fa
Damir <125348732+ki55el@users.noreply.github.com>
2023-06-01 14:28:28 +0600

    Update Shop.cs

--- Shop.cs
+++ Shop.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
@@ -49,54 +49,40 @@ class HelloWorld
             switch (num)
             {
                 case "1":
-
-                    var provide = from provider in providers
-                                  from delivery in provider.deliveries
-                                  from prod in delivery.products
-                                  group prod.name by provider.id;
-                    foreach (var group in provide)
-                    {
-                        Console.WriteLine(group.Key.ToString() + ':');
-                        foreach (var element in group)
-                        {
-                            Console.WriteLine('\t' + element.ToString());
-                        }
-                    }
+                    var by_provider = from provider in providers
+                        from delivery in provider.deliveries
+                        from product in delivery.products
+                        group product.name by provider.id.ToString();
+                    Printer(by_provider);
                     break;
 
                 case "2":
-                    var product = from provider in providers
-                                  from delivery in provider.deliveries
-                                  from prod in delivery.products
-                                  group provider.id by prod.name;
-                    foreach (var group in product)
-                    {
-                        Console.WriteLine(group.Key.ToString() + ':');
-                        foreach (var element in group)
-                        {
-                            Console.WriteLine('\t' + element.ToString());
-                        }
-                    }
+                    var by_product = from provider in providers
+                        from delivery in provider.deliveries
+                        from product in delivery.products
+                        group provider.id.ToString() by product.name;
+                    Printer(by_product);
                     break;
 
                 case "3":
-                    var date = from provider in providers
-                               from delivery in provider.deliveries
-                               from prod in delivery.products
-                               group prod.name by delivery.date;
-                    foreach (var group in date)
-                    {
-                        Console.WriteLine(group.Key.ToString() + ':');
-                        foreach (var element in group)
-                        {
-                            Console.WriteLine('\t' + element.ToString());
-                        }
-                    }
+                    var by_date = from provider in providers
+                        from delivery in provider.deliveries
+                        from product in delivery.products
+                        group product.name by delivery.date;
+                    Printer(by_date);
                     break;
             }
-            Console.WriteLine("\nДля продолжения нажмите любую клавишу . . .");
-            Console.ReadKey();
-            Console.Clear();
+        }
+    }
+    static void Printer(IEnumerable<IGrouping<string, string>> groups)
+    {
+        foreach (var group in groups)
+        {
+            Console.WriteLine(group.Key + ':');
+            foreach (var element in group)
+            {
+                Console.WriteLine('\t' + element);
+            }
         }
     }
 }
